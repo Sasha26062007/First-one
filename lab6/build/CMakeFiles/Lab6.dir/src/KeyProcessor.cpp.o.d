3cb56258f68020592e109230203ce2f53650f286
Sasha26062007 <panaetovs777@gmail.com>
2026-04-23 17:32:23 +0000

    fix: remove build directory from repository as per guidelines

--- lab6/build/CMakeFiles/Lab6.dir/src/KeyProcessor.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Lab6.dir/src/KeyProcessor.cpp.o: \
- /home/admin1/system_eng/lab_2/lab6/src/KeyProcessor.cpp \
- /usr/include/stdc-predef.h \
- /home/admin1/system_eng/lab_2/lab6/include/KeyProcessor.hpp
